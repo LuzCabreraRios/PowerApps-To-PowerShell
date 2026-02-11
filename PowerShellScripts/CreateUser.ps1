baf4eb65c9996df9d27f64185279c68ccf6ea41c
luz <luzrio0411@gmail.com>
2026-02-11 12:54:43 -0600

    new powershell

---
+++ PowerShellScripts/CreateUser.ps1
@@ -0,0 +1,17 @@
+# 1. Define the password
+$Password = ConvertTo-SecureString "l0ck3d" -AsPlainText -Force
+
+# 2. Create 'temp30' in the College/Users OU
+New-ADUser `
+    -Name "Temp 30" `
+    -GivenName "Temp" `
+    -Surname "30" `
+    -SamAccountName "temp30" `
+    -UserPrincipalName "temp30@wsc.local" `
+    -Path "OU=Users,OU=College,DC=wsc,DC=local" `
+    -AccountPassword $Password `
+    -Enabled $true `
+    -ChangePasswordAtLogon $false
+
+# 3. Confirm they exist
+Get-ADUser -Identity temp30
